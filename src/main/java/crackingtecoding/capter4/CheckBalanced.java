89adcab71af3f041b8fbeb67d6015182c3b5bedd
spcruzaley <sp_cruzaley@hotmail.com>
2021-01-14 16:10:36 -0600

    CrackingTheCodeingInterview - Capter 4.4

--- src/main/java/crackingtecoding/capter4/CheckBalanced.java
+++ src/main/java/crackingtecoding/capter4/CheckBalanced.java
@@ -2,31 +2,18 @@ package crackingtecoding.capter4;
 
 public class CheckBalanced {
 
-    public boolean isBalanced(Node node) {
-        int all = isBalanced(node, 0, 0, 0);
-        System.out.println("Final: " + all);
+    public boolean isBalanced(Node n) {
+        int left = getMaxHeight(n.left);
+        int right = getMaxHeight(n.right);
 
-        return false;
+        return Math.abs(left-right) <= 1;
     }
 
-    private int isBalanced(Node n, int l, int r, int res) {
-        if(n == null)
+    private int getMaxHeight(Node node) {
+        if(node == null)
             return 0;
 
-        isBalanced(n.left, l+1, r, res);
-        isBalanced(n.right, l, r+1, res);
-
-        res = Math.abs(l-r);
-        System.out.println("L-"+l+" | R-"+r+" = " + res);
-        return res;
+        return 1 + Math.max(getMaxHeight(node.left), getMaxHeight(node.right));
     }
 
-    public void preorder(Node node) {
-        if(node == null)
-            return;
-
-        preorder(node.left);
-        System.out.print(node.value + " - ");
-        preorder(node.right);
-    }
 }
--- src/test/java/crackingtecoding/capter4/CheckBalancedTest.java
+++ src/test/java/crackingtecoding/capter4/CheckBalancedTest.java
@@ -1,31 +1,55 @@
 package crackingtecoding.capter4;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class CheckBalancedTest {
 
     @Test
-    public void method() {
-        Node node = generateTree();
+    public void isBalancedAsValid() {
+        Node node = generateValidBalancedTree();
         CheckBalanced checkBalanced = new CheckBalanced();
+        boolean actualResult = checkBalanced.isBalanced(node);
 
-        //checkBalanced.isBalanced(node);
-        checkBalanced.preorder(node);
+        Assert.assertTrue(actualResult);
     }
 
-    private Node generateTree() {
+    @Test
+    public void isBalancedAsInvalid() {
+        Node node = generateInvalidBalancedTree();
+        CheckBalanced checkBalanced = new CheckBalanced();
+        boolean actualResult = checkBalanced.isBalanced(node);
+
+        Assert.assertFalse(actualResult);
+    }
+
+    private Node generateValidBalancedTree() {
         Node node = new Node(1);
 
         node.left = new Node(2);
         node.left.left = new Node(4);
-        node.left.right = new Node(7);
-        node.left.right.left = new Node(17);
-        node.left.right.left.left = new Node(117);
-        node.left.left.left = new Node(5);
+        node.left.right = new Node(4);
+        node.left.right.left = new Node(4);
 
         node.right = new Node(3);
         node.right.left = new Node(6);
-        //node.right.right = new Node(7);
+        node.right.right = new Node(7);
+
+        return node;
+    }
+
+    private Node generateInvalidBalancedTree() {
+        Node node = new Node(1);
+
+        node.left = new Node(2);
+        node.left.left = new Node(4);
+        node.left.right = new Node(4);
+        node.left.right.left = new Node(4);
+        node.left.right.left.right = new Node(4);
+
+        node.right = new Node(3);
+        node.right.left = new Node(6);
+        node.right.right = new Node(7);
 
         return node;
     }
